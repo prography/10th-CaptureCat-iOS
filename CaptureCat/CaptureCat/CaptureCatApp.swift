e48b974eba90ee6c9f9fd7bc767a6952288b6a29
mint3382 <mskim0926@naver.com>
2025-06-05 19:11:36 +0900

    [Setting] Basic (#1)
    * ⚙️(Setting) 프로젝트 파일 생성
    
    * ⚙️(setting) SwiftLint 적용

---
+++ CaptureCat/CaptureCat/CaptureCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureCatApp.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptureCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
