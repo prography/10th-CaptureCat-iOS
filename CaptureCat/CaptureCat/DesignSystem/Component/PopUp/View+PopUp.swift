21fde08c42297946c6785833b5ed837af896ed73
mint3382 <mskim0926@naver.com>
2025-06-23 09:26:06 +0900

    ✨(feat) CustomPopUpViewModifier로 구현
    - primary01로 네이밍 변경(중복 문제)

---
+++ CaptureCat/CaptureCat/DesignSystem/Component/PopUp/View+PopUp.swift
@@ -0,0 +1,31 @@
+//
+//  View+PopUp.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 6/23/25.
+//
+
+import SwiftUI
+
+extension View {
+    func popUp(
+        isPresented: Binding<Bool>,
+        title: String,
+        message: String,
+        cancelTitle: String,
+        confirmTitle: String,
+        confirmAction: @escaping () -> Void
+    ) -> some View {
+        modifier(
+            PopUpViewModifier(
+                isPresented: isPresented,
+                title: title,
+                message: message,
+                cancelTitle: cancelTitle,
+                confirmTitle: confirmTitle,
+                confirmAction: confirmAction
+            )
+        )
+    }
+}
+
