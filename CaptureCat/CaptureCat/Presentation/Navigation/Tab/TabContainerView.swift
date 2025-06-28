84eee998feba5e4126a0d78a3e979c6efcf52a83
mint3382 <mskim0926@naver.com>
2025-06-28 23:40:29 +0900

    ✨(feat) TabContainerView 구현

---
+++ CaptureCat/CaptureCat/Presentation/Navigation/Tab/TabContainerView.swift
@@ -0,0 +1,35 @@
+//
+//  TabContainerView.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 6/28/25.
+//
+
+import SwiftUI
+
+struct TabContainerView: View {
+    @State private var selectedTab: Tab = .home
+
+    var body: some View {
+        ZStack {
+            // 1) 탭별 화면 분기
+            Group {
+                switch selectedTab {
+                case .temporaryStorage:
+                    EmptyView()
+                case .home:
+                    HomeView()
+                case .tag:
+                    EmptyView()
+                }
+            }
+
+            // 2) 화면 아래에 탭 바
+            VStack {
+                Spacer()
+                CustomTabView(selectedTab: $selectedTab)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
