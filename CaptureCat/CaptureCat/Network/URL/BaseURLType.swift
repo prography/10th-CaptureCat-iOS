961bb87101df69137f953526f48ddd84d3b5b4bd
mint3382 <mskim0926@naver.com>
2025-07-14 10:18:20 +0900

    ✨(feat) BaseURL Type 구현

---
+++ CaptureCat/CaptureCat/Network/URL/BaseURLType.swift
@@ -0,0 +1,11 @@
+//
+//  BaseURLType.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/11/25.
+//
+
+enum BaseURLType: String {
+    case production = "https://api.capture-cat.com"
+    case development = "https://dev.api.capture-cat.com"
+}
