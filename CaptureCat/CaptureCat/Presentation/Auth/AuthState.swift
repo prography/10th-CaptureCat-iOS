94d815b90a34a178734c92d3cdb709be19ba326e
mint3382 <mskim0926@naver.com>
2025-07-17 00:29:59 +0900

    ✨(feat) AuthState 추가

---
+++ CaptureCat/CaptureCat/Presentation/Auth/AuthState.swift
@@ -0,0 +1,17 @@
+//
+//  AuthState.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/11/25.
+//
+
+import Foundation
+
+// 계정 상태 명시
+enum AuthenticationState {
+    case splash
+    case initial
+    case signIn
+    case guest
+    case start
+}
