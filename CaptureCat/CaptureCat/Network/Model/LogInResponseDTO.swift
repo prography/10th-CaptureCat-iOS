74946d32a96700929e36cfa8f4d537c19ef21035
mint3382 <mskim0926@naver.com>
2025-07-17 00:31:22 +0900

    ✨(feat) LogInResponseDTO 구현

---
+++ CaptureCat/CaptureCat/Network/Model/LogInResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  LogInResponseDTO.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/16/25.
+//
+
+import Foundation
+
+struct LogInResponseDTO: Decodable {
+    let result: String
+}
