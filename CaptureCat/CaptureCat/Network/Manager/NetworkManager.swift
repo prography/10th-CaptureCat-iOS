430b0bd10fc6b5c151192d40753a0bcca79755b3
mint3382 <mskim0926@naver.com>
2025-07-17 01:15:21 +0900

    ✨(feat) NetworkManager에 login용 함수 추가

--- CaptureCat/CaptureCat/Network/Manager/NetworkManager.swift
+++ CaptureCat/CaptureCat/Network/Manager/NetworkManager.swift
@@ -46,6 +46,45 @@ class NetworkManager {
         }
     }
     
+    func fetchLoginData<Builder: BuilderProtocol>(_ builder: Builder, isRetry: Bool = false) async throws -> Builder.Response {
+        let request = try await makeRequest(builder)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        if let string = String(data: data, encoding: .utf8) {
+            print("Response Body:", string)
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.responseNotFound
+        }
+        
+        if let accessToken = httpResponse.value(forHTTPHeaderField: "Authorization"),
+           let refreshToken = httpResponse.value(forHTTPHeaderField: "Refresh-Token") {
+            KeyChainModule.create(key: .accessToken, data: accessToken)
+            KeyChainModule.create(key: .refreshToken, data: refreshToken)
+        }
+        
+        debugPrint("httpResponse: \(httpResponse.statusCode)")
+        
+        switch httpResponse.statusCode {
+        case 200...299:
+            return try await builder.deserializer.deserialize(data)
+        case 400:
+            throw NetworkError.badRequest
+        case 401:
+            throw NetworkError.unauthorized
+        case 403:
+            throw NetworkError.forBidden
+        case 404:
+            throw NetworkError.responseNotFound
+        case 429:
+            throw NetworkError.tooManyRequests
+        case 500:
+            throw NetworkError.internalServerError
+        default:
+            throw NetworkError.unknown(httpResponse.statusCode)
+        }
+    }
+    
     // MARK: - Private
     private func makeRequest<Builder: BuilderProtocol>(_ builder: Builder) async throws -> URLRequest {
         let fullURL = baseURL.appendingPathComponent(builder.path)
@@ -70,6 +109,15 @@ class NetworkManager {
         
         if builder.method != .get {
             request.httpBody = try await builder.serializer.serialize(builder.parameters)
+        }
+        
+        debugPrint("✅ [Request Info]")
+        debugPrint("URL:", request.url?.absoluteString ?? "nil")
+        debugPrint("Method:", request.httpMethod ?? "nil")
+        debugPrint("Headers:", request.allHTTPHeaderFields ?? [:])
+        if let body = request.httpBody,
+           let bodyString = String(data: body, encoding: .utf8) {
+            debugPrint("Body:", bodyString)
         }
         
         return request
