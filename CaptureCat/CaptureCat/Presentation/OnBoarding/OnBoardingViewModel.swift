1797d74296b403e3ae9589197b6927bce1d446df
mint3382 <mskim0926@naver.com>
2025-07-16 21:27:46 +0900

    ✨(feat) OnBoardingView, ViewModel 추가

---
+++ CaptureCat/CaptureCat/Presentation/OnBoarding/OnBoardingViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  OnBoardingViewModel.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/16/25.
+//
+
+import SwiftUI
+
+@Observable
+class OnBoardingViewModel {
+    var currentPage: Int = 0
+    let onBoardingPages: [Page] =
+    [Page(description: "스크린샷만 쏙\n골라서 저장해요", image: .onBoard1, indicator: .indicator1, next: "다음"),
+         Page(description: "여러 장을 한 번에\n태그로 정리해요", image: .onBoard2, indicator: .indicator2, next: "다음"),
+         Page(description: "태그로 쉽게 찾고\n바로 활용해요", image: .onBoard3, indicator: .indicator3, next: "시작하기")]
+    var isOnBoarding: Bool = true
+    
+    func increasePage() {
+        if currentPage < onBoardingPages.count - 1 {
+            currentPage += 1
+        } else {
+            isOnBoarding = false
+        }
+    }
+
+    func decreasePage() {
+        if currentPage > 0 {
+            currentPage -= 1
+        }
+    }
+    
+    func skipOnBoarding() {
+        isOnBoarding = false
+    }
+}
+
+struct Page {
+    let description: String
+    let image: ImageResource
+    let indicator: ImageResource
+    let next: String
+}
