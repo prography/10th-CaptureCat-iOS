61a33a48339bedd3a211ac95e607d83d9a94f893
mint3382 <mskim0926@naver.com>
2025-07-17 01:16:53 +0900

    ✨(feat) RecommandLoginView 구현

---
+++ CaptureCat/CaptureCat/Presentation/Auth/RecommandLoginView.swift
@@ -0,0 +1,56 @@
+//
+//  RecommandLoginView.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/11/25.
+//
+
+import SwiftUI
+
+struct RecommandLoginView: View {
+    @ObservedObject var viewModel: AuthViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Button {
+                viewModel.authenticationState = .start
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(.text01)
+            }
+            .padding(.bottom, 16)
+            .padding(.top, 60)
+            .padding(.leading, 16)
+            
+            VStack(spacing: 8) {
+                Text("시작하기 전에")
+                    .multilineTextAlignment(.center)
+                    .CFont(.headline01Bold)
+                Text("로그인하면 모든 디바이스에서 관리하고,\n갤러리에서 지워도 캡처캣에 안전하게 보관돼요.")
+                    .multilineTextAlignment(.center)
+                    .CFont(.body01Regular)
+                
+                Spacer()
+                Image(.beforeStart)
+                Spacer()
+                
+                Button {
+                    viewModel.authenticationState = .initial
+                } label: {
+                    Text("로그인하기")
+                }
+                .primaryStyle()
+                .padding(.horizontal, 16)
+                Button {
+                    viewModel.authenticationState = .start
+                } label: {
+                    Text("나중에 하기")
+                        .CFont(.caption02Regular)
+                        .padding(.vertical, 8)
+                        .foregroundStyle(.text03)
+                }
+            }
+            .padding(.bottom, 60)
+        }
+    }
+}
