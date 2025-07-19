f5e526f55547bdab81b3de9a67de25f82d95911f
mint3382 <mskim0926@naver.com>
2025-07-19 21:25:57 +0900

    ✨(feat) ScreenshotItem, ScreenshotView 구현

---
+++ CaptureCat/CaptureCat/DesignSystem/Component/View/ScreenshotView.swift
@@ -0,0 +1,35 @@
+//
+//  ScreenshotView.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 7/19/25.
+//
+
+import SwiftUI
+
+struct ScreenshotView: View {
+    @Binding var item: ScreenshotItem
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            Group {
+                item.imageData.toImage()
+            }
+            .clipped()
+            
+            // 2. 왼쪽 하단에 태그들 표시
+            HStack(spacing: 4) {
+                ForEach(item.tags, id: \.self) { tag in
+                    Text(tag)
+                        .CFont(.caption01Semibold)
+                        .padding(.horizontal, 7.5)
+                        .padding(.vertical, 4.5)
+                        .background(Color.overlayDim)
+                        .foregroundColor(.white)
+                        .cornerRadius(4)
+                }
+            }
+            .padding(6)
+        }
+    }
+}
