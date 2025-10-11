58301c1b2542a9996919eed9713e90f6f4aed79a
mint3382 <mskim0926@naver.com>
2025-10-12 02:58:47 +0900

    ✨(feat) UserDefaults+ 구현

---
+++ CaptureCat/CaptureCat/Util/Extension/UserDefaults+.swift
@@ -0,0 +1,43 @@
+//
+//  UserDefaults+Extensions.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 1/15/25.
+//
+
+import Foundation
+
+// MARK: - UserDefaults Extensions for App Settings
+extension UserDefaults {
+    
+    /// 이미지 저장 후 원본 삭제 설정 값 가져오기 (기본값: true)
+    var deleteOriginalsAfterSave: Bool {
+        get {
+            // 처음 접근 시 기본값 true로 설정
+            if object(forKey: LocalUserKeys.deleteOriginalsAfterSave.rawValue) == nil {
+                set(true, forKey: LocalUserKeys.deleteOriginalsAfterSave.rawValue)
+                synchronize()
+                debugPrint("🔧 원본 이미지 삭제 설정 초기값 설정: true")
+                return true
+            }
+            return bool(forKey: LocalUserKeys.deleteOriginalsAfterSave.rawValue)
+        }
+        set {
+            set(newValue, forKey: LocalUserKeys.deleteOriginalsAfterSave.rawValue)
+            synchronize()
+            debugPrint("🔧 원본 이미지 삭제 설정 변경: \(newValue)")
+        }
+    }
+    
+    /// 선택된 토픽 목록 가져오기
+    var selectedTopics: [String] {
+        get {
+            return stringArray(forKey: LocalUserKeys.selectedTopics.rawValue) ?? []
+        }
+        set {
+            set(newValue, forKey: LocalUserKeys.selectedTopics.rawValue)
+            synchronize()
+            debugPrint("🔧 선택된 토픽 목록 변경: \(newValue)")
+        }
+    }
+}
