19f0999cfcf12017f80acda3e14215b9e756b37d
mint3382 <mskim0926@naver.com>
2025-11-03 23:06:19 +0900

    ✨(feat) LocalUserKeys 분리

---
+++ CaptureCat/CaptureCat/Util/LocalUserKeys.swift
@@ -0,0 +1,15 @@
+//
+//  LocalUserKeys.swift
+//  CaptureCat
+//
+//  Created by minsong kim on 10/31/25.
+//
+
+import SwiftUI
+
+enum LocalUserKeys: String {
+    case didImageDeleteBottomSheetPresented = "didImageDeleteBottomSheetPresented"
+    case selectedTopics = "selectedTopics"
+    case deleteOriginalsAfterSave = "deleteOriginalsAfterSave"
+    case taggedImageIds = "taggedImageIds"
+}
